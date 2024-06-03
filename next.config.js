41449c885240fc171f63eb91d2c6010f07dc93f9
rafael-vasconcellos <rafael.vasconcelos7497@gmail.com>
2024-06-02 23:45:29 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+    async headers() {
+        return [
+            {
+                // Aplica esses cabeçalhos a todas as rotas
+                source: '/(.*)', 
+                headers: [
+                    {
+                        // Remova ou configure X-Frame-Options para permitir que a página seja embutida em qualquer domínio
+                        key: 'X-Frame-Options',
+                        value: 'ALLOWALL',
+                    }, {
+                        // Configure a política de segurança de conteúdo para permitir iframes de qualquer origem
+                        key: 'Content-Security-Policy',
+                        value: "frame-ancestors 'self' *", 
+                    },
+                ],
+            },
+        ];
+    },
 }
 
 module.exports = nextConfig
